596f6e3544afea30156d712eaf4e789b12a9ce98
Droneship <81514881+Seifood@users.noreply.github.com>
2022-07-16 02:01:51 +0200

    Nuke

--- twrp_gauguin.mk
+++ twrp_gauguin.mk
@@ -38,9 +38,3 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_PACKAGES += \
     qcom_decrypt \
     qcom_decrypt_fbe
-
-# Recovery
-TARGET_RECOVERY_DEVICE_MODULES += \
-    libion
-RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so
